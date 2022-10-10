35000c43ad24c754d86a51c67d4c029681d2179e
Lee Roland <glroland@hotmail.com>
2022-10-09 21:01:50 -0400

    added request host based mapping

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
 
 cd online
 python -m SimpleHTTPServer 9901 &
+PID_ONLINE=$!
+echo Online Process ID = $PID_ONLINE
+
 cd ../offline
 python -m SimpleHTTPServer 9902 &
+PID_OFFLINE=$1
+echo Offline Process ID = $PID_OFFLINE
+
 cd ..
 sleep 1
 curl http://localhost:9901
@@ -13,16 +18,21 @@ cp haproxy.cfg /etc/haproxy/haproxy.cfg
 systemctl restart haproxy.service
 tail -50 /var/log/messages
 
+echo Pulling HAProxy - Should Say Online...
 curl http://localhost
-# should say online
 
 # kill 9901
+kill -9 $PID_ONLINE
+sleep 1
 
+echo Pulling HAProxy - Should Say Offline...
 curl http://localhost
-# should say offline
 
-# restart 9901
+cd online
+python -m SimpleHTTPServer 9901 &
+PID_ONLINE=$!
+echo Online Process ID = $PID_ONLINE
+sleep 1
 
+echo Pulling HAProxy - Should Say Online...
 curl http://localhost
-# should say online
-
